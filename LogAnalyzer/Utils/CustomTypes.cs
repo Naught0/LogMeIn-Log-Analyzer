45b82049227d3955a82dd461499a56556e463a63
James <4589250+Naught0@users.noreply.github.com>
2019-10-03 16:42:54 -0400

    add custom types

---
+++ LogAnalyzer/Utils/CustomTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAnalyzer.utils
+{
+    public class CustomTypes
+    {
+        public class FilterList
+        {
+            public List<string> items { get; set; }
+            public List<int> counts { get; set; }
+
+            public FilterList()
+            {
+                items = new List<string>();
+                counts = new List<int>();
+            }
+        }
+    }
+}
