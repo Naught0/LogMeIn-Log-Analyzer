24cda55309968ad1808d40fa5f7decadb1d5740b
James <>
2019-10-07 22:43:53 -0400

    add custom type for CheckedListBox

---
+++ LogAnalyzer/utils/Types.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAnalyzer.utils
+{
+    public class Types
+    {
+        public class CheckListItem<T>
+        {
+            public T Data { get; set; }
+            public string Display { get; set; }
+
+            public CheckListItem(string displayValue, T dataValue)
+            {
+                Data = dataValue;
+                Display = displayValue;
+            }
+
+            public override string ToString()
+            {
+                return Display;
+            }
+        }
+    }
+}
